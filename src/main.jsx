ad634dbed39d5bd0cd23ac9e50210a220a46fd74
romkalan <romkalan@mail.ru>
2025-04-29 20:44:25 +0300

    check posibilites of React and jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,24 @@
 import ReactDOM from 'react-dom/client';
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <div>
+        <button onClick={() => console.log('clicked')}>
+            <span>
+                Click me!
+            </span>
+        </button>
+        <button>
+            <span>
+                Click me!
+            </span>
+        </button>
+        <button>
+            <span>
+                Click me!
+            </span>
+        </button>
+    </div>
+);
