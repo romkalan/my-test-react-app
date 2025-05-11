129fe33c4c0b42b59c07db9deadb0c267687c29b
romkalan <romkalan@mail.ru>
2025-05-11 11:22:53 +0300

    work with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Layout from "./components/layout/Layout.jsx";
 import RestaurantPage from "./pages/RestaurantPage.jsx";
+import ReviewForm from "./components/reviewForm/ReviewForm.jsx";
 
 function App() {
     return (
         <Layout>
             <RestaurantPage/>
+            <ReviewForm/>
         </Layout>
     );
 }
